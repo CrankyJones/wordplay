7bcde1fe2b4048ba05690111715f436cf9903cb8
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-01-19 13:52:31 -0800

    Put in a missing # sign and adjusted the output
    Co-authored by: Elena Talanova <lena.talanova.ua@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $("#wordPlay").submit(function (event) {
+    event.preventDefault();
+
+    let sentence = $("input#sentence").val();
+
+    let words = sentence.split(' ');
+    let wordlist = [];
+    words.forEach(function (word) {
+      if (word.length >= 3) {
+        wordlist.push(word);
+      }
+    });
+    wordlist.reverse();
+    let output = wordlist.join(" & ");
+    alert(output);
+  });
+});
